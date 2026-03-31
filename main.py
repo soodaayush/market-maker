4db0379e0dce16ace393938bef7fbd28bbd43f26
soodaayush <aayush.sood@icloud.com>
2026-03-30 23:05:07 -0300

    Wrote fundamental code for Market Makler

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Market Maker
+import random
+import time
 
 # - Program is based on a ticker, where
 # 	- Mid prices are updated every tick to determine bid and ask using a specified spread (+- 0.5)
@@ -30,17 +32,78 @@
 # 		- PnL (Profit and Loss) = cash + inventory * mid price
 # 	- Next tick
 
-def ticker():
-    pass
+# Global Variables
+
+SPREAD = 0.1
+TICKER = 0
+MID_PRICE = 100
+CASH = 500
+INVENTORY = 100
+
+def increment_ticker():
+    global TICKER
+
+    TICKER += 1
 
 def fill_order_book():
-    pass
+    bid = round(MID_PRICE - (SPREAD / 2), 2)
+    ask = round(MID_PRICE + (SPREAD / 2), 2)
 
-def trade():
-    pass
+    order_book = {}
 
-def setup():
-    pass
+    order_book["bid"] = {"bid": bid, "quantity": 1}
+    order_book["ask"] = {"ask": ask, "quantity": 1}
 
-def record_metrics():
-    pass
+    return order_book
+
+def adjust_mid_price():
+    global MID_PRICE
+    random_num = round(random.uniform(-0.05, 0.05), 2)
+
+    MID_PRICE = round(MID_PRICE + random_num, 2)
+
+def trade(order_book):
+    global INVENTORY, CASH
+
+    trader_action = ""
+    price = 0
+    random_num = random.randint(0, 1)
+
+    if random_num == 0:
+        trader_action = "BUY"
+        ask_data = order_book["ask"]
+        price = ask_data.get("ask")
+        INVENTORY -= 1
+        CASH += price
+    elif random_num == 1:
+        trader_action = "SELL"
+        bid_data = order_book["bid"]
+        price = bid_data.get("bid")
+        INVENTORY += 1
+        CASH -= price
+
+    return trader_action, price
+
+def calculate_PnL():
+    PnL = CASH + (INVENTORY * MID_PRICE)
+    return PnL
+
+while True:
+    increment_ticker()
+    order_book = fill_order_book()
+    action, price = trade(order_book)
+    PnL = calculate_PnL()
+    adjust_mid_price()
+
+    print(f"Tick: {TICKER}")
+    print(f"Mid Price: {MID_PRICE}")
+    print(f"Market Maker Quote: Bid=${order_book['bid'].get('bid')} Ask=${order_book['ask'].get('ask')}")
+    print(f"Trader Action: {action}")
+    print(f"Trade Price: ${price}")
+    print(f"Inventory: {INVENTORY}")
+    print(f"Cash: ${round(CASH, 2)}")
+    print(f"PnL: ${round(PnL, 2)}")
+
+    print("\n")
+
+    time.sleep(5)
